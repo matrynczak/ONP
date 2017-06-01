a4131e05d73e63e3516f91a9d7cb13785efaed3e
matrynczak <matrynczak@gmail.com>
2017-06-01 23:10:25 +0200

    ver1.1

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ONP
+
+Wersja z wpisywaniem znaków po kolei, możliwe liczby 
+wielocyfrowe. Wersja lepsza niz na branchu master.
--- src/CalculatingONP.java
+++ src/CalculatingONP.java
@@ -9,49 +9,49 @@ public class CalculatingONP {
     public static void main(String[] args) {
 
         Stack<Double> stos = new Stack<>();
-        String token = getToken();
+        String token = "start";
 
-        Integer tokenCharLength = token.length();
+        System.out.print("Podaj ciag liczb wg odwrotnej notacji polskiej. Dozwolone znaki to liczby oraz +,-,*,/:\n " +
+                "Wpisanie znaku = spowoduje zakończenie i wypisanie zawartości stosu.\n");
 
-        double a;
-        double b;
-        Integer result;
+        while(!token.equals("=")) {
 
-        for (int i = 0; i < tokenCharLength; i++) {
-            Character tokenElement = token.charAt(i);
+            token = getToken();
 
-               if( tokenElement == '+') {
-                   stos.push(stos.pop() + stos.pop());
-               }
-               else if(tokenElement == '-') {
+            if (token.equals("+")||token.equals("-")||token.equals("*")||token.equals("/")||token.equals("=")) {
+                if (token.equals("+")) {
+                    stos.push(stos.pop() + stos.pop());
+                    System.out.print(stos + "\n");
+                }
+                else if (token.equals("-")) {
                     Double x = stos.pop();
                     Double y = stos.pop();
                     stos.push(y - x);
+                    System.out.print(stos + "\n");
                 }
-               else if (tokenElement == '*') {
+                else if (token.equals("*")) {
                     stos.push(stos.pop() * stos.pop());
+                    System.out.print(stos + "\n");
                 }
-                else if (tokenElement == '/') {
+                else if (token.equals("/")) {
                     Double xx = stos.pop();
                     Double yy = stos.pop();
                     stos.push(yy / xx);
+                    System.out.print(stos + "\n");
                 }
-                else if (tokenElement>='0' && tokenElement<='9') {
-                    String numer = tokenElement.toString();
-                    stos.push(Double.valueOf(numer));
-                    System.out.print(numer);
-                }
-                System.out.print(stos+"\n");
+            }
+            else {
+                    stos.push(Double.valueOf(token));
+                    System.out.print(stos + "\n");
+
+                 }
             }
         System.out.print(stos);
         }
 
         public static String getToken () {
-            System.out.print("Podaj ciag liczb wg odwrotnej notacji polskiej. Dozwolone znaki +,-,*,/:\n");
             Scanner s = new Scanner(System.in);
             String token = s.next();
             return token;
         }
-
-
-    }
+}
